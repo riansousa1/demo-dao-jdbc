d20dd756c1cd6c0ab14da45872ca43707f669276
Rian Sousa <riansousa2507@gmail.com>
2026-04-10 06:55:59 -0300

    Entidade seller criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
-		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Rian", "riansousa@gmail.com", new Date(), 2000.0, obj);
+	
+		System.out.println(seller);
 
 	}
 
