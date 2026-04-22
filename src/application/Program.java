b3c041120f9e64552848b8042a1ccdf80de7c2e5
Rian Sousa <riansousa2507@gmail.com>
2026-04-22 07:12:55 -0300

    Método insert implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -35,6 +36,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TESTE 4 insert ===");
+		
+		Seller newSeller = new Seller(null, "Joao", "joao@gmail", new Date(), 2000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Insert! new Id = " + newSeller.getId());
 		
 		
 	}
